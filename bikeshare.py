952d2156c7430005ea6d7116706e8b740a163450
Jacqueline Hungenbach <jacqueline.hungenbach@bayer.com>
2021-11-29 07:23:43 +0100

    Update docstring station_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -144,7 +144,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular stations and trips."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
